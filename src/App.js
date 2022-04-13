73ce7e9690cf61e154f029c98654565b90b1445d
Vitaliy1804 <shervit82@gmail.com>
2022-04-13 21:57:01 +0300

    add to local storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { nanoid } from 'nanoid';
 import NotesList from "./components/NotesList";
+import Search from "./components/Search";
+import Header from './components/Header';
 
 const App = () => {
+
+  // let text = " "
+  // let speech = new SpeechSynthesisUtterance()
+
+  // function TextToSpeech() {
+  //   speech.text = text
+  //   speech.rate =0.5
+  //   speech.volume =1
+  //   speech.pitch =3
+  //   speech.lang="ru-UA"
+  //   window.speechSynthesis.speak(speech)
+  // }
+  // TextToSpeech()
+
   const [notes, setNotes] = useState([
     {
     id: nanoid(),
@@ -25,9 +41,58 @@ const App = () => {
     date: "04/05/2022"
   },
 ])
-  return <div className="container">
-    <NotesList notes={notes} />
+
+const [searchText, setSearchText] = useState('')
+
+const [darkMode, setDarkMode] = useState(false)
+
+useEffect(() => {
+  const savedNotes = JSON.parse(
+    localStorage.getItem('react-notes-app-data'))
+     
+    if (savedNotes) {
+      setNotes(savedNotes)
+    }
+}, [])
+
+useEffect(() => {
+  localStorage.setItem(
+    'react-notes-app-data',
+    JSON.stringify(notes)
+  )
+}, [notes])
+ 
+const addNote = (text) => {
+  const date = new Date();
+  const newNote = {
+    id: nanoid(),
+    text: text,
+    date: date.toLocaleString()
+  }
+  const newNotes = [...notes, newNote]
+  setNotes(newNotes)
+}
+
+const deleteNote = (id) => {
+  const newNotes = notes.filter((note)=> note.id !== id)
+  setNotes(newNotes)
+}
+
+  return (
+    <div className={`${darkMode && 'dark-mode'}`}>
+      <div className="container">
+    <Header handleToggleDarkMode={setDarkMode} />
+    <Search handleSearchNote={setSearchText} />
+    <NotesList 
+        notes={notes.filter((note) => 
+          note.text.toLowerCase().includes(searchText)
+        )} 
+        handleAddNote={addNote}
+        handleDeleteNote={deleteNote}
+    />
   </div>
+    </div>
+  )
 }
 
 export default App;
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,6 +1,7 @@
 import Note from './Note';
+import AddNote from './AddNote';
 
-const NotesList = ({notes}) => {
+const NotesList = ({notes, handleAddNote, handleDeleteNote}) => {
     return(
         <div className="notes-list">
             {notes.map((note) => (
@@ -8,8 +9,10 @@ const NotesList = ({notes}) => {
             id = {note.id}
             text = {note.text}
             date = {note.date}
+            handleDeleteNote = {handleDeleteNote}
             /> 
             ))}
+            <AddNote handleAddNote={handleAddNote} />
         </div>
     )
 }
